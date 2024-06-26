d564bc174f7ce90d8edd781da20f3a2aaea8d1ae
anajmb <estrelaanamb0108@gmail.com>
2024-06-26 11:46:41 -0300

    Exercícios utilizando funções base

--- 26-06-24/exer1.js
+++ 26-06-24/exer1.js
@@ -1,4 +1,4 @@
-//Exercio 1 
+//Exercício 1 
 const tom = "Tom"; 
 const firstTom = "Hanks"; 
 const secondTom = "Cruise"; 
@@ -6,11 +6,26 @@ const myFavoriteTom = "Tom " + "Cruise.";
 
 console.log("Meu nome e sobrenome favorito: " + myFavoriteTom);
 
-//Exercicio 2 
+//Exercício 2 
 const programmingLanguage = "JavaScript"; 
 const js = programmingLanguage[0] + programmingLanguage[4];
 
 
 console.log(js);
+
+//Exercício 3 
+const winterMessage = "o inverno está chegando"; 
+
+console.log(winterMessage.toUpperCase());
+
+//Exercício 4 
+console.log(winterMessage.indexOf("v"));
+
+//Exercício 5 
+console.log(winterMessage.lastIndexOf("n"));
+
+
+
+
 
 
---
+++ 26-06-24/textos2.js
@@ -0,0 +1,11 @@
+let txt = "Estão chegando as provas"; // a contagem começa no 0 
+
+console.log(txt); 
+console.log(txt.length); // Quantidade de caracteres no valor da String 
+console.log(txt.indexOf("as")); // Retorna a primeira posição na String que tem "as"  
+console.log(txt.lastIndexOf("as")); // Retorna a última posição na String que tem "as" 
+console.log(txt.slice(0, 5)); // Dividir, pegar um pedaços, ou seja, letras que estão dentro do intervalo de números
+console.log(txt.replace("provas", "férias")); // Trocar uma palavra por outra 
+console.log(txt.toUpperCase()); // Deixar em maiusculo 
+console.log(txt.toLowerCase()); // Deixar em minusculo 
+
