2a00dd6d763cba4a37f0e6e7b3c674716894a537
anajmb <estrelaanamb0108@gmail.com>
2024-06-28 11:43:42 -0300

    Array

--- 28-06-24/for.js
+++ 28-06-24/for.js
@@ -1,3 +1,11 @@
 for(let i = 0; i < 3; i++) {
     console.log(`Agora i vale: ${i}`);
 }
+
+let frutas = ["maçã", "banana", "pera"]; 
+
+//console.table(frutas);
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(` - ${frutas[i]}`);
+}
