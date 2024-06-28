f66885ad3984d4ce58dcc3945308775e898d00e7
anajmb <estrelaanamb0108@gmail.com>
2024-06-28 10:40:25 -0300

    Criando sistema de notas

--- 27-06-24/sistema-aluno/script.js
+++ 27-06-24/sistema-aluno/script.js
@@ -1,14 +1,37 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
+
+//Criando uma função para limpar o campo
+function clearForm() {
+   inputNota.value = "";
+}
+
+
 //criando uma função
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(inputNota.value);
-
- if(inputNota.value >= 6) { 
+    const valorDaNota = inputNota.value;
+    //console.log(inputNota.value);
+   
+    //Validação de formulário
+   if(valorDaNota === '') { 
+      alert("Campo obrigátorio");
+      return false; 
+   }
+   
+    //Lógica 
+ if(valorDaNota >= 6) { 
     alert("Você foi aprovado");
  } else { 
     alert("Você foi reprovado");
  }
+   
+ //Chamando a função limpar campo
+ clearForm();
+
+
+
+
 
 });
+
